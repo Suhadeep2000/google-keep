08384ffd6c11de7c88628c7a277f83c0d44debc9
Subhadeep Das <abhisubhadeep@gmail.com>
2023-01-10 06:59:52 +0530

    v1.1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      <p>Ⓒ Made By Subhadeep Das</p>
+    </div>
+  );
+}
+
+export default Footer;
